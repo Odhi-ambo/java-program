1179a49fbce1833caea6c38cf0fd2b62f8889578
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-20 16:50:47 +0300

    addition of two # using swing element

--- addNumbers.java
+++ addNumbers.java
@@ -7,6 +7,14 @@ public class addNumbers {
         //jframe
         JFrame frame = new JFrame("Add Two Numbers");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.setLayout(null);
+
+
+        //label
+        JLabel label1 = new JLabel("Enter first number: ");
+        label1.setBounds(50, 20, 150, 30);
+        frame.add(label1);
 
     }
 }
