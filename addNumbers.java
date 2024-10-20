22932e597e01cc8b8f868948619560f6fe4a1ca8
Odhi-ambo <michaelderrick605@gmail.com>
2024-10-20 16:49:37 +0300

    addition of two # using swing element

---
+++ addNumbers.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+
+
+public class addNumbers {
+    public static void main(String[] args) {
+        //jframe
+        JFrame frame = new JFrame("Add Two Numbers");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
